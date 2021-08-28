78d44da9b35236ee840737ddc59a5b8db78367d4
alanqrwang <alanqingranwang@gmail.com>
2021-08-28 13:34:51 -0400

    small edit to bash script

--- scripts/train_sup.sh
+++ scripts/train_sup.sh
@@ -2,4 +2,12 @@
 
 source activate aw847-torch
 
-python run.py -fp no_rescalein_feature --epochs 10000 --batch_size 32 --lr 1e-3 --unet_hdim 32 --hnet_hdim 64 --undersampling_rate 16p3 --loss_list l1 ssim --sampling uhs
+python run.py -fp stringify_loss_prefix \
+  --num_epochs 10000 \
+  --batch_size 32 \
+  --lr 1e-3 \
+  --unet_hdim 32 \
+  --hnet_hdim 64 \
+  --undersampling_rate 16p3 \
+  --loss_list l1 ssim \
+  --method uhs
