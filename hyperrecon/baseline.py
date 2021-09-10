c508667334aef3964d39b3273a0e07d12c6e1057
alanqrwang <alanqingranwang@gmail.com>
2021-09-10 15:39:06 -0400

    baseline set metrics

--- hyperrecon/baseline.py
+++ hyperrecon/baseline.py
@@ -1,5 +1,5 @@
 import torch
-import time
+import numpy as np
 from tqdm import tqdm
 
 from hyperrecon.util.train import BaseTrain
@@ -38,3 +38,29 @@ class Baseline(BaseTrain):
   def set_eval_hparams(self):
     self.val_hparams = torch.tensor(self.hyperparameters).view(-1, 1)
     self.test_hparams = torch.tensor(self.hyperparameters).view(-1, 1)
+  
+  def set_metrics(self):
+    self.list_of_metrics = [
+      'loss:train',
+      'psnr:train',
+    ]
+    self.list_of_val_metrics = [
+      'loss:val:' + self.stringify_list(l.tolist()) for l in self.val_hparams
+    ] + [
+      'psnr:val:' + self.stringify_list(l.tolist()) for l in self.val_hparams
+    ]
+    self.list_of_test_metrics = [
+      'loss:test:' + self.stringify_list(l.tolist()) + ':sub{}'.format(s) for l in self.test_hparams for s in np.arange(self.num_val_subjects)
+    ] + [
+      'psnr:test:' + self.stringify_list(l.tolist()) + ':sub{}'.format(s) for l in self.test_hparams for s in np.arange(self.num_val_subjects)
+    ] + [
+      'ssim:test:' + self.stringify_list(l.tolist()) + ':sub{}'.format(s) for l in self.test_hparams for s in np.arange(self.num_val_subjects)
+    ] + [
+      'hfen:test:' + self.stringify_list(l.tolist()) + ':sub{}'.format(s) for l in self.test_hparams for s in np.arange(self.num_val_subjects)
+    ] + [
+      'watson:test:' + self.stringify_list(l.tolist()) + ':sub{}'.format(s) for l in self.test_hparams for s in np.arange(self.num_val_subjects)
+    ] + [
+      'mae:test:' + self.stringify_list(l.tolist()) + ':sub{}'.format(s) for l in self.test_hparams for s in np.arange(self.num_val_subjects)
+    # ] + [
+    #   'dice:test:' + self.stringify_list(l.tolist()) + ':sub{}'.format(s) for l in self.test_hparams for s in np.arange(self.num_val_subjects)
+    ]
