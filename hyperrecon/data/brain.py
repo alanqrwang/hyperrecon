fixed scaling gt during DC computation

hyperrecon/data/brain.py
@@ -222,8 +222,8 @@ class VolumeDataset(data.Dataset):
 
 
 def get_train_gt(img_dims='160_224'):
-  # gt_path = '/share/sablab/nfs02/users/aw847/data/brain/adrian/brain_train_normalized.npy'
-  gt_path = '/share/sablab/nfs02/users/aw847/data/brain/adrian/segs/train_mri_normalized_{img_dims}.npy'.format(img_dims=img_dims)
+  gt_path = '/share/sablab/nfs02/users/aw847/data/brain/adrian/brain_train_normalized.npy'
+  # gt_path = '/share/sablab/nfs02/users/aw847/data/brain/adrian/segs/train_mri_normalized_{img_dims}.npy'.format(img_dims=img_dims)
   gt = np.load(gt_path)
   return gt
 

hyperrecon/data/mask.py
@@ -57,7 +57,12 @@ class EPIVertical(BaseMask):
     # mask[:, random_line_idx] = 1.
     # print('Generated EPI Vertical with mean', mask.mean())
 
-    path = '/share/sablab/nfs02/users/aw847/data/masks/EPI_vertical_{rate}_percentacs50_{dim1}_{dim2}.npy'.format(rate=self.rate, dim1=self.dims[0], dim2=self.dims[1])
+    if self.rate == '4':
+      path = '/share/sablab/nfs02/users/aw847/data/masks/EPI_vertical_{rate}_percentacs50_{dim1}_{dim2}.npy'.format(rate=self.rate, dim1=self.dims[0], dim2=self.dims[1])
+    elif self.rate == '8':
+      path = '/share/sablab/nfs02/users/aw847/data/masks/EPI_vertical_{rate}_percentacs50_{dim1}_{dim2}.npy'.format(rate=self.rate, dim1=self.dims[0], dim2=self.dims[1])
+    else:
+      raise ValueError('no mask path found')
     print('Loading mask:', path)
     mask = np.load(path)
     mask = np.fft.fftshift(mask)

hyperrecon/uniform.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 import random
 from hyperrecon.util.train import BaseTrain
 from hyperrecon.util.metric import bpsnr
@@ -20,7 +21,8 @@ class Uniform(BaseTrain):
 
   def set_eval_hparams(self):
     self.val_hparams = torch.tensor([0., 1.]).view(-1, 1)
-    self.test_hparams = torch.tensor([0., 0.1, 0.25, 0.5, 1.]).view(-1, 1)
+    # self.test_hparams = torch.tensor([0., 0.25, 0.5, 0.75, 1.]).view(-1, 1)
+    self.test_hparams = torch.tensor(np.linspace(0, 0.01, 20)).float().view(-1, 1)
     # self.val_hparams = torch.tensor([[0.,0.], [1.,1.]])
     # hparams = []
     # for i in np.linspace(0, 1, 50):

hyperrecon/util/metric.py
@@ -4,6 +4,7 @@ import math
 import scipy.ndimage as nd
 from unetsegmentation import test as segtest
 from perceptualloss.loss_provider import LossProvider
+from hyperrecon.util import utils
 
 def psnr(img1, img2):
   '''PSNR of two images.  
@@ -19,19 +20,18 @@ def psnr(img1, img2):
   psnr = 20 * math.log10(max_pixel / math.sqrt(mse))
   return psnr
 
-def bpsnr(bimg1, bimg2):
+def bpsnr(bimg1, bimg2, normalized=False):
   '''Compute average PSNR of a batch of images (possibly complex).
 
   Args:
     bimg1: (batch_size, c, n1, n2)
     bimg2: (batch_size, c, n1, n2)
   '''
-  bimg1 = bimg1.norm(dim=1)
-  bimg2 = bimg2.norm(dim=1)
-  # if normalized:
-  #     recons = rescale(recons)
-  #     gt = rescale(gt)
-  #     zf = rescale(zf)
+  bimg1 = bimg1.norm(dim=1, keepdim=True)
+  bimg2 = bimg2.norm(dim=1, keepdim=True)
+  if normalized:
+      bimg1 = utils.unit_rescale(bimg1)
+      bimg2 = utils.unit_rescale(bimg2)
 
   metrics = []
   for img1, img2 in zip(bimg1, bimg2):