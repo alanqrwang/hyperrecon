9bd78669bacd5877efe9383e8218c381aabbb3a5
alanqrwang <alanqingranwang@gmail.com>
2021-10-19 10:03:47 -0400

    improvements for loupe

--- hyperrecon/loupe.py
+++ hyperrecon/loupe.py
@@ -20,52 +20,48 @@ class Loupe(BaseTrain):
     self.val_hparams = torch.tensor(1/float(self.undersampling_rate)).view(-1, 1)
     self.test_hparams = torch.tensor(1/float(self.undersampling_rate)).view(-1, 1)
   
-  def compute_loss(self, pred, gt, *args, **kwargs):
-    del args, kwargs
+  def compute_loss(self, pred, gt):
     return nn.MSELoss()(pred, gt)
   
+  def prepare_batch(self, batch):
+    targets = batch.view(-1, 1, *batch.shape[-2:]).float().to(self.device)
+    bs = len(targets)
+    return targets, bs
+
   def inference(self, zf, *args):
     del args
     return self.network(zf)
 
-  def train_step(self, targets, segs):
+  def train_step(self, batch):
     '''Train for one step.'''
-    batch_size = len(targets)
-
-    targets, segs = targets.float().to(self.device), segs.float().to(self.device)
+    targets, batch_size = self.prepare_batch(batch)
     self.optimizer.zero_grad()
     with torch.set_grad_enabled(True):
-      pred, _, _ = self.inference(targets)
+      pred, _ = self.inference(targets)
       loss = self.compute_loss(pred, targets, None, None, None)
       loss.backward()
       self.optimizer.step()
     psnr = bpsnr(targets, pred)
     return loss.cpu().detach().numpy(), psnr, batch_size
   
-  def eval_step(self, targets, segs, hparams):
+  def eval_step(self, batch, hparams):
     '''Eval for one step.
     
     Args:
       batch: Single batch from dataloader
       hparams: Single hyperparameter vector (1, num_hyperparams)
     '''
-    targets, segs = targets.float().to(self.device), segs.float().to(self.device)
-    targets = targets.view(-1, 1, *targets.shape[-2:])
-    segs = segs.view(-1, 1, *targets.shape[-2:])
+    targets, _ = self.prepare_batch(batch)
     with torch.set_grad_enabled(False):
-      pred, measurement, measurement_ft = self.inference(targets)
+      pred, measurement = self.inference(targets)
 
-    return measurement, measurement_ft, targets, pred, segs, hparams
+    return measurement, targets, pred, hparams
   
 class LoupeAgnostic(BaseTrain):
   """RateAgnostic."""
 
   def __init__(self, args):
     super(LoupeAgnostic, self).__init__(args=args)
-
-  def sample_hparams(self, num_samples):
-    '''Samples hyperparameters from distribution.'''
-    return torch.FloatTensor(num_samples, self.num_hparams).uniform_(0, 1)
 
   def set_eval_hparams(self):
     self.val_hparams = torch.tensor([0., 1.]).view(-1, 1)
@@ -74,44 +70,44 @@ class LoupeAgnostic(BaseTrain):
   def compute_loss(self, pred, gt, *args, **kwargs):
     del args, kwargs
     return nn.MSELoss()(pred, gt)
-  
-  def train_step(self, targets, segs):
-    '''Train for one step.'''
-    batch_size = len(targets)
-    hparams = self.sample_hparams(batch_size).to(self.device)
 
-    targets, segs = targets.float().to(self.device), segs.float().to(self.device)
+  def prepare_batch(self, batch):
+    targets = batch.view(-1, 1, *batch.shape[-2:]).float().to(self.device)
+    bs = len(targets)
+    return targets, bs
+  
+  def train_step(self, batch):
+    '''Train for one step.'''
+    targets, batch_size = self.prepare_batch(batch)
+    hparams = self.sample_hparams(batch_size).to(self.device)
     self.optimizer.zero_grad()
     with torch.set_grad_enabled(True):
-      pred, _, _ = self.inference(targets, hparams)
-      loss = self.compute_loss(pred, targets, None, None, None)
+      pred, _ = self.inference(targets, hparams)
+      loss = self.compute_loss(pred, targets)
       loss.backward()
       self.optimizer.step()
     psnr = bpsnr(targets, pred)
     return loss.cpu().detach().numpy(), psnr, batch_size
   
-  def eval_step(self, targets, segs, hparams):
+  def eval_step(self, batch, hparams):
     '''Eval for one step.
     
     Args:
       batch: Single batch from dataloader
       hparams: Single hyperparameter vector (1, num_hyperparams)
     '''
-    targets, segs = targets.float().to(self.device), segs.float().to(self.device)
-    targets = targets.view(-1, 1, *targets.shape[-2:])
-    segs = segs.view(-1, 1, *targets.shape[-2:])
-    batch_size = len(targets)
+    targets, batch_size = self.prepare_batch(batch)
     hparams = hparams.repeat(batch_size, 1).to(self.device)
     with torch.set_grad_enabled(False):
-      pred, measurement, measurement_ft = self.inference(targets, hparams)
+      pred, inputs = self.inference(targets, hparams)
 
-    return measurement, measurement_ft, targets, pred, segs, hparams
+    return inputs, targets, pred, hparams
   
   def test(self, save_preds=False):
     for hparam in self.test_hparams:
       hparam_str = self.stringify_list(hparam.tolist())
       print('Testing with hparam', hparam_str)
-      zf, gt, y, pred, seg, coeffs = self.get_predictions(hparam, by_subject=True)
+      zf, gt, pred, _ = self.get_predictions(hparam, by_subject=True)
       for i in range(len(zf)):
         # Save predictions to disk
         if save_preds:
@@ -125,7 +121,7 @@ class LoupeAgnostic(BaseTrain):
             np.save(zf_path, zf[i].cpu().detach().numpy())
         for key in self.test_metrics:
           if 'loss' in key and hparam_str in key and 'sub{}'.format(i) in key:
-            loss = self.compute_loss(pred[i], gt[i], y[i], seg[i], coeffs[i], is_training=False)
+            loss = self.compute_loss(pred[i], gt[i])
             loss = self.process_loss(loss).item()
             self.test_metrics[key].append(loss)
           elif 'psnr' in key and hparam_str in key and 'sub{}'.format(i) in key:
@@ -138,6 +134,6 @@ class LoupeAgnostic(BaseTrain):
             self.test_metrics[key].append(bwatson(gt[i], pred[i]))
           elif 'mae' in key and hparam_str in key and 'sub{}'.format(i) in key:
             self.test_metrics[key].append(bmae(gt[i], pred[i]))
-          elif 'dice' in key and hparam_str in key and 'sub{}'.format(i) in key:
-            loss_roi, _,_,_,_ = dice(pred[i], gt[i], seg[i])
-            self.test_metrics[key].append(float(loss_roi.mean()))
+          # elif 'dice' in key and hparam_str in key and 'sub{}'.format(i) in key:
+          #   loss_roi, _,_,_,_ = dice(pred[i], gt[i], seg[i])
+          #   self.test_metrics[key].append(float(loss_roi.mean()))
---
+++ hyperrecon/model/loupeunet.py
@@ -0,0 +1,138 @@
+import torch
+import torch.nn as nn
+
+from hyperrecon.data.mask import Loupe, ConditionalLoupe
+from hyperrecon.util.forward import CSMRIForward
+from hyperrecon.model.unet import Unet
+from hyperrecon.model.hypernetwork import HyperNetwork
+from hyperrecon.util import utils
+
+class LoupeUnet(nn.Module):
+  """LoupeUnet"""
+  def __init__(self, in_ch, out_ch, h_ch, image_dims, undersampling_rate, residual=True, use_batchnorm=False):
+    """
+    Args:
+      in_units_hnet : Input dimension for hypernetwork
+      h_units_hnet : Hidden dimension for hypernetwork
+      in_ch_main : Input channels for Unet
+      out_ch_main : Output channels for Unet
+      h_ch_main : Hidden channels for Unet
+      residual : Whether or not to use residual U-Net architecture
+    """
+    super(LoupeUnet, self).__init__()
+
+    self.undersampling_rate = 1 / torch.tensor(float(undersampling_rate)).cuda()
+    self.unet = Unet(
+                    in_ch=in_ch, 
+                    out_ch=out_ch, 
+                    h_ch=h_ch, 
+                    residual=residual,
+                    use_batchnorm=use_batchnorm
+                )
+    self.loupe = Loupe(image_dims).cuda()
+    self.forward_model = CSMRIForward()
+
+  def forward(self, x):
+    """
+    Args:
+      x : Input (batch_size, 2, img_height, img_width)
+    """
+    batch_size = len(x)
+    rate = self.undersampling_rate.repeat(batch_size)
+    undersample_mask = self.loupe(batch_size, rate)
+    measurement, measurement_ft = self.forward_model.generate_measurement(x, undersample_mask)
+    out = self.unet(measurement)
+    return out, measurement, measurement_ft
+  
+class LoupeHyperUnet(nn.Module):
+  """HyperUnet for hyperparameter-agnostic image reconstruction"""
+  def __init__(self, in_units_hnet, h_units_hnet, in_ch_main, out_ch_main, h_ch_main, image_dims, residual=True, use_batchnorm=False):
+    """
+    Args:
+      in_units_hnet : Input dimension for hypernetwork
+      h_units_hnet : Hidden dimension for hypernetwork
+      in_ch_main : Input channels for Unet
+      out_ch_main : Output channels for Unet
+      h_ch_main : Hidden channels for Unet
+      residual : Whether or not to use residual U-Net architecture
+    """
+    super(LoupeHyperUnet, self).__init__()
+
+    # HyperNetwork
+    self.hnet = HyperNetwork(
+                    in_dim=in_units_hnet, 
+                    h_dim=h_units_hnet
+                )
+    self.unet = Unet(
+                    in_ch=in_ch_main, 
+                    out_ch=out_ch_main, 
+                    h_ch=h_ch_main, 
+                    hnet_hdim=h_units_hnet,
+                    residual=residual,
+                    use_batchnorm=use_batchnorm
+                )
+    self.loupe = Loupe(image_dims).cuda()
+    self.forward_model = CSMRIForward()
+
+  def forward(self, x, hyperparams):
+    """
+    Args:
+      x : Input (batch_size, 2, img_height, img_width)
+      hyperparams : Hyperparameter values (batch_size, num_hyperparams)
+    """
+    batch_size = len(x)
+    undersample_mask = self.loupe(batch_size, hyperparams)
+    measurement = self.forward_model(x, undersample_mask)
+    inputs = utils.ifft(measurement)
+    hyp_out = self.get_hyp_out(hyperparams)
+    out = self.unet(inputs, hyp_out)
+    return out, inputs
+  
+  def get_hyp_out(self, hyperparams):
+    return self.hnet(hyperparams)
+
+class ConditionalLoupeHyperUnet(nn.Module):
+  """HyperUnet for hyperparameter-agnostic image reconstruction"""
+  def __init__(self, in_units_hnet, h_units_hnet, in_ch_main, out_ch_main, h_ch_main, image_dims, residual=True, use_batchnorm=False):
+    """
+    Args:
+      in_units_hnet : Input dimension for hypernetwork
+      h_units_hnet : Hidden dimension for hypernetwork
+      in_ch_main : Input channels for Unet
+      out_ch_main : Output channels for Unet
+      h_ch_main : Hidden channels for Unet
+      residual : Whether or not to use residual U-Net architecture
+    """
+    super(ConditionalLoupeHyperUnet, self).__init__()
+
+    # HyperNetwork
+    self.hnet = HyperNetwork(
+                    in_dim=in_units_hnet, 
+                    h_dim=h_units_hnet
+                )
+    self.unet = Unet(
+                    in_ch=in_ch_main, 
+                    out_ch=out_ch_main, 
+                    h_ch=h_ch_main, 
+                    hnet_hdim=h_units_hnet,
+                    residual=residual,
+                    use_batchnorm=use_batchnorm
+                )
+    self.loupe = ConditionalLoupe(image_dims).cuda()
+    self.forward_model = CSMRIForward()
+
+  def forward(self, x, hyperparams):
+    """
+    Args:
+      x : Input (batch_size, 2, img_height, img_width)
+      hyperparams : Hyperparameter values (batch_size, num_hyperparams)
+    """
+    undersample_mask = self.loupe(hyperparams)
+    measurement = self.forward_model(x, undersample_mask)
+    inputs = utils.ifft(measurement)
+    hyp_out = self.get_hyp_out(hyperparams)
+    out = self.unet(inputs, hyp_out)
+    return out, inputs
+  
+  def get_hyp_out(self, hyperparams):
+    return self.hnet(hyperparams)
