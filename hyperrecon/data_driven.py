5fd0d3592a34ee2353e194f0ea30898b978bad22
alanqrwang <alanqingranwang@gmail.com>
2021-10-25 09:13:14 -0400

    added dhs support

--- hyperrecon/data_driven.py
+++ hyperrecon/data_driven.py
@@ -8,19 +8,10 @@ class DataDriven(BaseTrain):
   def __init__(self, args):
     super(DataDriven, self).__init__(args=args)
 
-  def sample_hparams(self, num_samples):
-    '''Samples hyperparameters from distribution.'''
-    return torch.FloatTensor(num_samples, self.num_hparams).uniform_(0, 1)
-
-  def process_loss(self, loss):
+  def process_loss(self, loss, loss_dict):
     dc_losses = loss_dict['dc']
     _, perm = torch.sort(dc_losses) # Sort by DC loss, low to high
-    sort_losses = self.losses[perm] # Reorder total losses by lowest to highest DC loss
-    hyperparams = hyperparams[perm]
+    sort_losses = loss[perm] # Reorder total losses by lowest to highest DC loss
     loss = torch.mean(sort_losses[:self.topK]) # Take only the losses with lowest DC
 
     return loss
-
-  def train_epoch_begin(self):
-      super().train_epoch_begin()
-      print('DHS Sampling')
